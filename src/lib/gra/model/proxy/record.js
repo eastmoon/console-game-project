ce2433629892e315fb8555426f8fd9d3614c07e6
jacky.chen <jacky.chen@yxunistar.com>
2018-02-21 11:32:18 +0800

    Modify record file algorithm.

--- src/lib/gra/model/proxy/record.js
+++ src/lib/gra/model/proxy/record.js
@@ -2,11 +2,11 @@
 import glob from "glob";
 import Lowdb from "lowdb";
 import FileSync from "lowdb/adapters/FileSync";
-import Moment from "moment";
-import Crypto from "crypto-js";
-import SHA256 from "crypto-js/sha256";
 import AES from "crypto-js/aes";
 import EncUtf8 from "crypto-js/enc-utf8";
+
+// utils
+import {emptyVariable} from "lib/gra/utils/verifity";
 
 /*
 Game record file proxy model.
@@ -22,61 +22,88 @@ export default class RecordProxy {
         this._name = $name;
         this._container = $container;
         this._file = null;
+        this._original = null;
+        this._lock = false;
     }
     // Method
     load($path, $callback) {
         glob($path, {}, (err, matches) => {
             if (!err && matches.length >= 0 && matches[0] === $path) {
                 const adapter = new FileSync($path, {
-                    deserialize: (string) => {
-                        console.debug("RecordProxy deserialize");
-                        console.debug(string);
-                        return JSON.parse(string);
-                    }
+                    serialize: this._encrypt.bind(this),
+                    deserialize: this._decrypt.bind(this)
                 });
                 this._file = Lowdb(adapter);
-
-                    /*
-                    .then((db) => {
-                        console.debug(this._file.get("data"));
-                        this._file.get("data").assign("12345").write();
-                        console.debug(this._file.get("data"));
-                        console.log(db);
-                        return db;
-                    });
-                    */
+                const save = {
+                    x: 1,
+                    y: 2,
+                    z: 8
+                };
+                this.save(save);
                 if (this._container !== null && typeof this._container.register === "function") {
                     this._container.register(this._name, this._file.value());
-                    const save = {
-                        x: 1,
-                        y: 2,
-                        z: 3
-                    };
-                    //this._file.write();
                 }
                 $callback();
             }
-        });
-            /*
-                    const key = "1234567890abcdefg";
-                    const skey = SHA256(key).toString();
-                    console.log("Key : ", skey);
 
-                    const save = {
-                        key: skey,
-                        data: {
-                            x: 1,
-                            y: 2,
-                            z: 3
-                        }
-                    }
-                    const ciphertext = AES.encrypt(JSON.stringify(save.data), save.key);
-                    console.log(ciphertext.toString());
-                    var bytes  = AES.decrypt(ciphertext.toString(), save.key);
-                    var decryptedData = JSON.parse(bytes.toString(EncUtf8));
-                    console.log(decryptedData);
-            $resolve($progress);
+
+
         });
-        */
+    }
+
+    save($save = null) {
+        if (!emptyVariable($save)) {
+            this._file
+              .set("data", $save)
+              .write();
+        }
+    }
+
+    _encrypt(object) {
+        //
+        if (this._lock) {
+            console.debug("Record Proxy lock, file will not change.");
+            return this._original;
+        }
+        if (!emptyVariable(object)) {
+            let key = object.key;
+            if ( typeof key === "string") {
+                let ciphertext = AES.encrypt(JSON.stringify(object.data), key);
+                object.data = ciphertext.toString();
+                return JSON.stringify(object);
+            } else {
+                console.error("Record Proxy error, key isn't exist.");
+                return this._original;
+            }
+        }
+    }
+
+    _decrypt(content) {
+        //
+        this._original = content;
+        this._lock = false;
+        //
+        let object = JSON.parse(content);
+        let key = object.key;
+        if ( typeof key === "string" && typeof object.data === "string") {
+            try {
+                let bytes  = AES.decrypt(object.data, key);
+                object.data = JSON.parse(bytes.toString(EncUtf8));
+            } catch(err) {
+                if (typeof object.data === "string" && object.data === "") {
+                    // error outcome, because data is initial state.
+                    this._lock = false;
+                    console.warn(`Record Proxy warn, save file is initial state.`);
+                } else {
+                    // error outcome, it could key error, data error, etc.
+                    this._lock = true;
+                    console.error(`Record Proxy error, decrypt isn't success.`);
+                }
+            }
+        } else {
+            this._lock = true;
+            console.error(`Record Proxy error, record file structure is wrong.`);
+        }
+        return object;
     }
 }
